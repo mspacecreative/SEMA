bc8f9c711d7d3a75455a8a4717b2cabed49969ae
Matt <matt@mspacecreative.com>
2019-04-06 16:37:38 -0300

    c

--- wp-content/themes/sema/js/isotope.js
+++ wp-content/themes/sema/js/isotope.js
@@ -9,20 +9,6 @@ $(window).load(function () {
 	    }
 	});
 	
-	// filter functions
-	var filterFns = {
-	  // show if number is greater than 50
-	  numberGreaterThan50: function(itemElem) {
-	    var number = itemElem.querySelector(".number").textContent;
-	    return parseInt(number, 10) > 50;
-	  },
-	  // show if name ends with -ium
-	  ium: function(itemElem) {
-	    var name = itemElem.querySelector(".name").textContent;
-	    return name.match(/ium$/);
-	  }
-	};
-	
 	// bind filter button click
 	var filtersElem = document.querySelector(".filters-button-group");
 	filtersElem.addEventListener("click", function(event) {
