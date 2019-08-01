40813d50c54c0eedff7a01acb7cd1329ba3408e6
Matt <matt@mspacecreative.com>
2019-08-01 11:56:55 -0300

    c

--- wp-content/themes/sema/js/scripts.js
+++ wp-content/themes/sema/js/scripts.js
@@ -139,7 +139,7 @@ jQuery(document).ready(function($) {
 		$('.chat-now-button').on('click', function (e) {
 			e.preventDefault();
 			//$('body').addClass('open-chat-window');
-			$('.widget-launcher').trigger("click");
+			$('#hubspot-messages-iframe-container button').trigger("click");
 			
 			/*if ( $('body').hasClass('open-chat-window') ) {
 				$('.shadow').addClass('active');
