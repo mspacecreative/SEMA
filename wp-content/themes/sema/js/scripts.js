2cecfaee5bb376fa95a40bc72032548fa9260f4b
Matt <matt@mspacecreative.com>
2019-08-01 11:02:36 -0300

    c

--- wp-content/themes/sema/js/scripts.js
+++ wp-content/themes/sema/js/scripts.js
@@ -139,9 +139,10 @@ jQuery(document).ready(function($) {
 		$('.chat-now-button').click(function (e) {
 			e.preventDefault();
 			$('body').addClass('open-chat-window');
-			if ( $('body').hasClass('open-chat-window') ) {
-				$('shadow').addClass('active');
-			}
 		});
+		
+		if ( $('body').hasClass('open-chat-window') ) {
+			$('.shadow').addClass('active');
+		}
 		
 });
