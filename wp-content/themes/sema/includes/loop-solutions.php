67a2a86b5d667d4c635d95b07024a1e55c816033
Matt <matt@mspacecreative.com>
2019-08-05 10:13:19 -0300

    c

---
+++ wp-content/themes/sema/includes/loop-solutions.php
@@ -0,0 +1,21 @@
+<?php
+$loop = new WP_Query( array( 'post_type' => 'solutions', 'posts_per_page' => -1 ) );
+    if ( $loop->have_posts() ) :
+        while ( $loop->have_posts() ) : $loop->the_post(); ?>
+        <div class="solutions_grid_box">
+			<h4><?php the_title(); ?></h4>
+			<span><strong>BEST FOR</strong></span><br />
+			
+			<?php if( have_rows('grid_content_box') ): 
+			
+				while( have_rows('grid_content_box') ): the_row(); ?>
+				<p><?php the_sub_field('best_for_content'); ?></p>
+				<a class="et_pb_button hide-on-desktop" style="font-size: 15px;" href="#<?php the_sub_field('learn_more_button'); ?>">LEARN MORE</a>
+				<?php endwhile; ?>
+				
+			<?php endif; ?>
+			
+		</div>
+        <?php endwhile; ?>
+    <?php endif;
+wp_reset_postdata(); ?>
