f546b94a5ab69e71f1cf8d1669b280aae7df5643
Matt <matt@mspacecreative.com>
2019-08-05 11:08:53 -0300

    c

--- wp-content/themes/sema/includes/loop-solutions.php
+++ wp-content/themes/sema/includes/loop-solutions.php
@@ -5,7 +5,7 @@ $loop = new WP_Query( array( 'post_type' => 'solutions', 'posts_per_page' => -1 
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
         <div class="solutions_grid_box">
 			<h3><?php the_title(); ?></h3>
-			<span><strong>BEST FOR</strong></span><br />
+			<span><strong>BEST FOR</strong></span>
 			
 			<?php if( have_rows('grid_content_box') ): 
 			
