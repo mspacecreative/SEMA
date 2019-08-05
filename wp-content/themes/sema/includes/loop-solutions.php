12988df91affaa81eeb7407397c4fed68005b6e4
Matt <matt@mspacecreative.com>
2019-08-05 10:23:05 -0300

    c

--- wp-content/themes/sema/includes/loop-solutions.php
+++ wp-content/themes/sema/includes/loop-solutions.php
@@ -1,7 +1,7 @@
 <?php
 $loop = new WP_Query( array( 'post_type' => 'solutions', 'posts_per_page' => -1 ) );
     if ( $loop->have_posts() ) : ?>
-    <div class="solutions_grid">
+    <div class="solutions_grid clearfix">
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
         <div class="solutions_grid_box">
 			<h4><?php the_title(); ?></h4>
