7d93c630ce4ef422d003b18638571bc128a16741
Matt <matt@mspacecreative.com>
2019-08-05 09:31:38 -0300

    c

--- wp-content/themes/sema/js/scripts.js
+++ wp-content/themes/sema/js/scripts.js
@@ -144,16 +144,4 @@ jQuery(document).ready(function($) {
 				console.log('Please install HubSpot WordPress plugin');
 			}
 		});
-		
-		// Get an array of all element heights
-		var elementHeights = $('.solutions_grid > .et_pb_module').map(function() {
-			return $(this).outerHeight();
-		}).get();
-		
-		// Math.max takes a variable number of arguments
-		// `apply` is equivalent to passing each height as an argument
-		var maxHeight = Math.max.apply(null, elementHeights);
-		
-		// Set each height to the max height
-		$('.solutions_grid > .et_pb_module').outerHeight(maxHeight);
 });
