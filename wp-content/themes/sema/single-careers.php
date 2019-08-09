b71e23e36cd9c226543a2ce5de7f304cd611a0d0
Matt <matt@mspacecreative.com>
2019-08-09 08:06:59 -0300

    c

--- wp-content/themes/sema/single-careers.php
+++ wp-content/themes/sema/single-careers.php
@@ -170,7 +170,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 			<?php endwhile; ?>
 			</div> <!-- #left-area -->
 
-			<?php get_sidebar('sidebar-careers'); ?>
+			<?php get_sidebar('careers'); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
 	<?php endif; ?>
