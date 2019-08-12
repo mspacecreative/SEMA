36ae756febd5ef9f25283f5ae467b95fc844ef17
Matt <matt@mspacecreative.com>
2019-08-12 13:02:02 -0300

    c

--- wp-content/plugins/isotope/js/isotope.js
+++ wp-content/plugins/isotope/js/isotope.js
@@ -20,7 +20,7 @@
         if (window_top > top) {
             $('.filters-button-group, .mobile-filter').addClass('fixed');
             $('#stick-here').css('height', $('#main-header').height() + 35);
-            $('#stick-here-mobile').css('height', $(this).siblings('.mobile-filter').outerHeight());
+            $('#stick-here-mobile').css('height', $('#stick-here-mobile').siblings('.mobile-filter').outerHeight());
             $('.filters-button-group, .mobile-filter').css('top', $('#main-header').height());
             if ( $('.mobile-filter').css('display') == 'block' ) {
                 $('.filters-button-group').css('top', $('#main-header').height() + $('.mobile-filter').outerHeight());
