1821fa32edf3f4473e357a56557f4eb8c5eafaeb
Matt <matt@mspacecreative.com>
2019-08-14 07:28:18 -0300

    c

--- wp-content/themes/sema/includes/loop-resources.php
+++ wp-content/themes/sema/includes/loop-resources.php
@@ -20,7 +20,7 @@ if ( $arr_posts->have_posts() ) :
         $arr_posts->the_post(); ?>
         
         <div class="featured-resource-container clearfix max-width-800">
-			<h3><?php esc_html_e('Featured Resource: '); ?><?php the_title(); ?></h3>
+			<h3><?php esc_html_e('Featured Resource: '); ?><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h3>
 			<div class="two_third">
 				<?php the_excerpt(); ?>
 			</div>
