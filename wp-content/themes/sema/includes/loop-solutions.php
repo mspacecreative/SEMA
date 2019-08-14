a018e6ca36d1a691f37d946a20f09b72f5c1e6c8
Matt <matt@mspacecreative.com>
2019-08-14 15:46:22 -0300

    c

--- wp-content/themes/sema/includes/loop-solutions.php
+++ wp-content/themes/sema/includes/loop-solutions.php
@@ -71,6 +71,8 @@ $loop = new WP_Query( array( 'post_type' => 'solutions', 'posts_per_page' => -1,
 			
 			<a class="et_pb_button hide-on-desktop" style="font-size: 16px;" href="<?php the_sub_field('internal_button_link'); ?>"><?php the_sub_field('cta_button_label'); ?></a>
 			
+			<a class="et_pb_button hide-on-desktop" style="font-size: 16px; margin-left: 15px;" href="<?php echo home_url('get-a-demo'); ?>"><?php esc_html_e('Get a Demo'); ?></a>
+			
 			<?php endwhile;
 			endif; ?>
 			
