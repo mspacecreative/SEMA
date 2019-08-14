4df1ed73aeb2638bd0790b94385c0d1c36c163f0
Matt <matt@mspacecreative.com>
2019-08-14 07:35:52 -0300

    c

--- wp-content/plugins/isotope/includes/sema-resource-cards-isotope.php
+++ wp-content/plugins/isotope/includes/sema-resource-cards-isotope.php
@@ -100,15 +100,15 @@ endif; wp_reset_query(); ?>
 								?>
 								<?php if ( get_field('anchor_link') ): ?>
 								<div>
-									<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="color: #ffffff; padding: .3em 2em .3em .7em; background-color: #cc9901; transition: 0.25s ease-in-out; margin-top: 15px; display: inline-block;" href="/solutions-2/#<?php the_field('anchor_link'); ?>"><?php _e('Learn More'); ?></a>
+									<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="margin-top: 15px; display: inline-block;" href="/solutions-2/#<?php the_field('anchor_link'); ?>"><?php _e('Learn More'); ?></a>
 								</div>
 								<?php elseif ( get_field('external_link') ): ?>
 								<div>
-									<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="color: #ffffff; padding: .3em 2em .3em .7em; background-color: #cc9901; transition: 0.25s ease-in-out; margin-top: 15px; display: inline-block;" href="<?php the_field('external_link'); ?>" target="_blank"><?php _e('Learn More'); ?></a>
+									<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="margin-top: 15px; display: inline-block;" href="<?php the_field('external_link'); ?>" target="_blank"><?php _e('Learn More'); ?></a>
 								</div>
 								<?php else : ?>
 								<div>
-									<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="color: #ffffff; padding: .3em 2em .3em .7em; background-color: #cc9901; transition: 0.25s ease-in-out; margin-top: 15px; display: inline-block;" href="<?php the_permalink(); ?>"><?php _e('Learn More'); ?></a>
+									<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="margin-top: 15px; display: inline-block;" href="<?php the_permalink(); ?>"><?php _e('Learn More'); ?></a>
 								</div>
 								<?php endif; ?>
 							</div>
