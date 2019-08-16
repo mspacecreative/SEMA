b37693d2ec3283404a97a11eef77c7cb4c0559b6
Matt <matt@mspacecreative.com>
2019-08-16 07:39:19 -0300

    c

--- wp-content/themes/sema/single-resources.php
+++ wp-content/themes/sema/single-resources.php
@@ -87,9 +87,10 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 				</article> <!-- .et_pb_post -->
 
 			<?php endwhile; ?>
+			
+			<?php get_sidebar('resources'); ?>
 			</div> <!-- #left-area -->
 
-			<?php get_sidebar('resources'); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
 	<?php endif; ?>
