76961105860472bea7c1af5a8526b468e072ed5d
Matt <matt@mspacecreative.com>
2019-08-19 14:14:08 -0300

    c

--- wp-content/plugins/isotope/js/isotope.js
+++ wp-content/plugins/isotope/js/isotope.js
@@ -19,7 +19,8 @@
         var top = $('#stick-here, #stick-here-mobile').offset().top;
         if (window_top > top) {
             $('.filters-button-group, .mobile-filter').addClass('fixed');
-            $('#stick-here').css('height', $('.button-group').outerHeight() + 100);
+            //$('#stick-here').css('height', $('.button-group').outerHeight() + 100);
+			$('.iso-grid').css('margin-top', '50px');
             $('.filters-button-group, .mobile-filter').css('top', $('#main-header').height());
             if ( $('.mobile-filter').css('display') == 'block' ) {
                 $('.filters-button-group').css('top', $('#main-header').height() + $('.mobile-filter').outerHeight());
