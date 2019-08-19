3e7a382b9e66e3d45d4b5d4fa73a22f4ebc5b74f
Matt <matt@mspacecreative.com>
2019-08-19 14:46:02 -0300

    c

--- wp-content/plugins/isotope/js/isotope.js
+++ wp-content/plugins/isotope/js/isotope.js
@@ -16,7 +16,7 @@
 	
 	function sticktothetop() {
         var window_top = $(window).scrollTop();
-        var top = $('#stick-here, #stick-here-mobile').offset().top;
+        var top = $('.button-group, #stick-here-mobile').offset().top;
         if (window_top > top) {
             $('.filters-button-group, .mobile-filter').addClass('fixed');
             $('#stick-here').css('height', '105px');
