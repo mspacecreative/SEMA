1f1a3098fa44a6a457b8a4f2ed7a6641c0f66d58
Matt <matt@mspacecreative.com>
2019-08-19 17:15:00 -0300

    c

--- wp-content/plugins/isotope/js/isotope.js
+++ wp-content/plugins/isotope/js/isotope.js
@@ -38,7 +38,7 @@
 	function moveSolutionsImage() {
 		var img = $('.solutions_blurbs .solution-thumb');
 		if ( img.css('top') == 'auto') {
-			img.appendTo('h2');
+			img.insertAfter($this).parent();
 		}
 	}
     
