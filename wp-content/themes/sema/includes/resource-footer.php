05e7ef2290e6d72335114a46d8d885adb04ab288
Matt <matt@mspacecreative.com>
2019-08-21 10:41:27 -0300

    c

--- wp-content/themes/sema/includes/resource-footer.php
+++ wp-content/themes/sema/includes/resource-footer.php
@@ -27,7 +27,9 @@ if ( $arr_posts->have_posts() ) :
         $arr_posts->the_post(); ?>
         
         <div class="featured-resource-footer clearfix">
-			<?php if ( get_field('resource_button_type') == 'external' ): ?>
+			<?php
+			$external = get_field('resource_button_type') == 'external' );
+			if ( $external ): ?>
 			<h2><?php esc_html_e('Featured Resource: '); ?><a style="color: #0072d6;" href="<?php the_field('external_link'); ?>" target="_blank"><?php the_title(); ?></a></h2>
 			<?php else : ?>
 			<h2><?php esc_html_e('Featured Resource: '); ?><a style="color: #0072d6;" href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -37,7 +39,11 @@ if ( $arr_posts->have_posts() ) :
 			else :
 			the_excerpt();
 			endif; ?>
+			<?php if ( $external ): ?>
+			<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="display: inline-block;" href="<?php the_field('external_link'); ?>" target="_blank"><?php _e('Learn More'); ?></a>
+			<?php else : ?>
 			<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="display: inline-block;" href="<?php the_permalink(); ?>"><?php _e('Learn More'); ?></a>
+			<?php endif; ?>
 		</div>
         
     <?php endwhile;
