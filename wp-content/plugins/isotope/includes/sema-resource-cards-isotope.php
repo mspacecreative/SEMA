a137b7d942dfd0f852c80672fa159c98d700eca7
Matt <matt@mspacecreative.com>
2019-09-04 08:17:50 -0300

    c

--- wp-content/plugins/isotope/includes/sema-resource-cards-isotope.php
+++ wp-content/plugins/isotope/includes/sema-resource-cards-isotope.php
@@ -122,64 +122,12 @@ endif; wp_reset_query(); ?>
 							</div>
 							<?php else : ?>
 							<div>
-								<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="margin-top: 15px; display: inline-block;" href="<?php echo the_permalink(); ?>"><?php _e('Learn More'); ?></a>
-							</div>
-							<?php endif; ?>
-						</div>
-					</div>
-					<?php elseif ( get_field('resource_image_type') == 'image' ): ?>
-					<div class="grid-inner">
-						<div class="resource-bg-img">
-						    <?php if ( has_post_thumbnail() ) { ?>
-								<?php if ( get_field('resource_button_type') == 'external' ): ?>
-								<a href="<?php the_field('external_link'); ?>" target="_blank">
-								    <?php echo the_post_thumbnail(); ?>
-								</a>
-								<?php elseif ( get_field('resource_button_type') == 'internal' ): ?>
-								<a href="<?php the_field('internal_page'); ?>">
-								    <?php echo the_post_thumbnail(); ?>
-								</a>
-						        <?php else : ?>
-								<a href="<?php the_permalink(); ?>">
-							        <?php echo the_post_thumbnail(); ?>
-						        </a>
-								<?php endif; ?>
-						    <?php } ?>
-						</div>
-						<div class="card-content">
-							<?php 
-							$terms = get_the_terms( $post->ID, 'types' );
-							if ( $terms ) { ?>
-							<p style="padding: 0; margin-bottom: 15px; font-size: 14px; display: block;">
-							<?php 
-								foreach ( $terms as $term ) {
-									echo esc_html_e('Resource Type: '); echo '<a href="'.get_term_link($term).'">' . $term->name . '</a>';
-								} ?>
-							</p>
-							<?php } ?>
-							<h3><?php the_title(); ?></h3>
-							<?php
-							if( has_excerpt() ) { 
-								the_excerpt(); 
-							} else {
-								the_content();
-							}
-							?>
-							<?php if ( get_field('resource_button_type') == 'internal' ): ?>
-							<div>
-								<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="margin-top: 15px; display: inline-block;" href="<?php the_field('internal_page'); ?>"><?php _e('Learn More'); ?></a>
-							</div>
-							<?php elseif ( get_field('resource_button_type') == 'external' ): ?>
-							<div>
-								<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="margin-top: 15px; display: inline-block;" href="<?php the_field('external_link'); ?>" target="_blank"><?php _e('Learn More'); ?></a>
-							</div>
-							<?php else : ?>
-							<div>
 								<a class="et_pb_button et_pb_custom_button_icon" data-icon="E" style="margin-top: 15px; display: inline-block;" href="<?php the_permalink(); ?>"><?php _e('Learn More'); ?></a>
 							</div>
 							<?php endif; ?>
 						</div>
 					</div>
+	
 					<?php elseif ( get_field('resource_image_type') == 'ebook' ): ?>
 					<div class="grid-inner">
 						<div class="ebook-cover">
@@ -233,6 +181,7 @@ endif; wp_reset_query(); ?>
 							<?php endif; ?>
 						</div>
 					</div>
+					
 					<?php elseif ( get_field('resource_image_type') == 'datasheet' ): ?>
 					<div class="grid-inner">
 						<?php if ( has_post_thumbnail() ) { ?>
@@ -284,6 +233,7 @@ endif; wp_reset_query(); ?>
 							<?php endif; ?>
 						</div>
 					</div>
+					
 					<?php else : ?>
 					<div class="grid-inner">
 						<div class="resource-bg-img">
